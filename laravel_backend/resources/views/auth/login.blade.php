e06ace29051c613c4868aa6252605ebec8bc0e1b
acaca6 <chndrana06@gmail.com>
2026-04-16 02:08:21 +0700

    Done tampilan tinggal tambahin printilan

--- laravel_backend/resources/views/auth/login.blade.php
+++ laravel_backend/resources/views/auth/login.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login — SIMOPANG</title>
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
     <link href="{{ asset('css/auth-base.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
