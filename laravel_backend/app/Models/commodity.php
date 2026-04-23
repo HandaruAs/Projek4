07b85e36500902cc7bef70ea5418e8c485fc5c58
Handaru <handaruarioseto12@gmail.com>
2026-04-23 15:50:06 +0700

    AI

--- laravel_backend/app/Models/commodity.php
+++ laravel_backend/app/Models/commodity.php
@@ -53,4 +53,8 @@ class Commodity extends Model
         return $this->hasMany(Prediction::class, 'commodity_id');
     }
 
+    public function trainingJobs()
+    {
+        return $this->hasMany(TrainingJob::class, 'commodity_id');
+    }
 }
