dda72201ec9fe21e48044902b21ebb581ea1222e
pallpall03 <valhidayat01@gmail.com>
2026-04-28 14:29:49 +0700

    fixxxxx

--- laravel_backend/app/Models/commodity.php
+++ laravel_backend/app/Models/commodity.php
@@ -8,6 +8,23 @@ class Commodity extends Model
 {
     protected $connection = 'mongodb';
     protected $collection = 'commodities';
+
+    /**
+     * Master data komoditas pangan di Jember.
+     * Contoh: Beras Medium, Bawang Merah, Cabai Rawit, Minyak Goreng
+     *
+     * Struktur dokumen MongoDB:
+     * {
+     *   _id: ObjectId,
+     *   name: "Beras Medium",   // unique index
+     *   category_id: ObjectId,
+     *   unit: "kg",             // satuan harga (kg, liter, dll)
+     *   stok_unit: "kuintal",   // satuan stok
+     *   description: "...",
+     *   created_at: ...,
+     *   updated_at: ...
+     * }
+     */
     protected $fillable = [
         'name',
         'category_id',
@@ -36,4 +53,8 @@ class Commodity extends Model
         return $this->hasMany(Prediction::class, 'commodity_id');
     }
 
+    public function trainingJobs()
+    {
+        return $this->hasMany(TrainingJob::class, 'commodity_id');
+    }
 }
